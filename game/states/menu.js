23ae5103f7e210bba22a46f61909cd461bc980fb
George Zeng <george.x.zeng@gmail.com>
2014-09-04 21:07:33 -0400

    tried including node modules

--- game/states/menu.js
+++ game/states/menu.js
@@ -1,36 +1,61 @@
+ /* Full tutorial: http://codevinsky.ghost.io/phaser-2-0-tutorial-flappy-bear-part-1/ */
+  'use strict';
+  function Menu() {}
 
-'use strict';
-function Menu() {}
+  Menu.prototype = {
+    preload: function() {
 
-Menu.prototype = {
-  preload: function() {
+    },
+    create: function() {
+      // add the background sprite
+      this.background = this.game.add.sprite(0,0,'background');
 
-  },
-  create: function() {
-    this.background = this.game.add.sprite(0, 0, 'background');
-    this.ground = this.game.add.tileSprite(0, 400, 335, 112, 'ground');
-    this.ground.autoScroll(-200, 0);
+      // add the ground sprite as a tile
+      // and start scrolling in the negative x direction
+      this.ground = this.game.add.tileSprite(0,400, 335,112,'ground');
+      this.ground.autoScroll(-200,0);
 
-    this.titleGroup = this.game.add.group();
+      /** STEP 1 **/
+      // create a group to put the title assets in
+      // so they can be manipulated as a whole
+      this.titleGroup = this.game.add.group()
 
-    this.title = this.game.add.sprite(0,0,'title');
-    this.titleGroup.add(this.title);
+      /** STEP 2 **/
+      // create the title sprite
+      // and add it to the group
+      this.title = this.add.sprite(0,0,'title');
+      this.titleGroup.add(this.title);
 
-    this.bird = this.game.add.sprite(200,5,'bird');
-    this.titleGroup.add(this.bird);
+      /** STEP 3 **/
+      // create the bear sprite
+      // and add it to the title group
+      this.bear = this.add.sprite(200,5,'bear');
+      this.titleGroup.add(this.bear);
 
-    this.bird.animations.add('flap');
-    this.bird.animations.play('flap', 12, true);
+      /** STEP 4 **/
+      // add an animation to the bear
+      // and begin the animation
+      this.bear.animations.add('flap');
+      this.bear.animations.play('flap', 12, true);
 
-    this.titleGroup.x = 30;
-    this.titleGroup.y = 0;
+      /** STEP 5 **/
+      // Set the originating location of the group
+      this.titleGroup.x = 30;
+      this.titleGroup.y = 100;
 
-    this.game.add.tween(this.titleGroup).to({y:15}, 350, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
+      /** STEP 6 **/
+      //  create an oscillating animation tween for the group
+      this.game.add.tween(this.titleGroup).to({y:115}, 350, Phaser.Easing.Linear.NONE, true, 0, 1000, true);
 
-  },
-  update: function() {
+      // add our start button with a callback
+      this.startButton = this.game.add.button(this.game.width/2, 300, 'startButton', this.startClick, this);
+      this.startButton.anchor.setTo(0.5,0.5);
+    },
+    startClick: function() {
+      // start button click handler
+      // start the 'play' state
+      this.game.state.start('play');
+    }
+  };
 
-  }
-};
-
-module.exports = Menu;
+  module.exports = Menu;
