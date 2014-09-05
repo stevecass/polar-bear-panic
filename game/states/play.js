ce2741e77d14f9ae98a6277b3ece26d6c235b1b3
Alexander Le <alextle0125@gmail.com>
2014-09-05 13:48:05 -0400

    added ground and ground physics

--- game/states/play.js
+++ game/states/play.js
@@ -2,6 +2,7 @@
   'use strict';
 
   var Bear = require('../prefabs/bear');
+  var Ground = require('../prefabs/ground');
   // var game = new Phaser.Game();
   var cursors;
   var bear;
@@ -14,12 +15,22 @@
 
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
       this.game.physics.arcade.gravity.y = 300;
+
+      // SETTING BOUNDS
+      this.game.world.setBounds(0, 0, 6400, 600);
+
       // this.game.physics.arcade.gravity.x = -200;
       this.background = this.game.add.sprite(0,0,'background');
 
       this.bear = new Bear(this.game, 100, this.game.height/2);
 
+      // CREATING AND ADDING A NEW GROUND
+      this.ground = new Ground(this.game, 0, 400, 800, 200);
+      this.game.add.existing(this.ground);
+
       this.game.add.existing(this.bear);
+
+      this.game.camera.follow(this.bear);
 
       this.game.input.keyboard.addKeyCapture([
         Phaser.Keyboard.RIGHT,
@@ -43,13 +54,12 @@
       var jump = this.input.keyboard.addKey(Phaser.Keyboard.UP);
       jump.onDown.add(this.bear.jump, this.bear);
 
-      // this.input.onDown.add(this.bear.jump, this.bear);
+      this.input.onDown.add(this.bear.jump, this.bear);
 
      },
 
     update: function() {
-
-
+      this.game.physics.arcade.collide(this.bear, this.ground, this.deathHandler, null, this);
     },
     clickListener: function() {
       this.game.state.start('gameover');
