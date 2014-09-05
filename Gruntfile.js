df7e6c16e95013890e6f13600c3819ae42ad1abd
Matthew Bunday <zenceph@gmail.com>
2014-09-05 19:31:54 +0000

    Change port on gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,7 +33,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 3456,
         // change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
       },
