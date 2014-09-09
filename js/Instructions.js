7e82925bcc28bd31a13100f30de34cacff31d765
George Zeng <george.x.zeng@gmail.com>
2014-09-09 16:30:17 -0400

    finished adding in states and instructions

---
+++ js/Instructions.js
@@ -0,0 +1,18 @@
+Instructions = function(game) {};
+
+Instructions.prototype = {
+	create: function() {
+		this.spacebar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+		label = this.game.add.text(400, 300, 
+			'Run and jump ahead of global warming to the finish line \n using your arrow keys to dodge the flying fish, icebergs,\n and ice lakes. Press SPACE to go back to the MENU.',{ fill: '#fff', align: 'center'});
+		// label.anchor.setTo(0.5, 0.5);
+		},
+
+	update: function() {
+		if (this.spacebar.isDown)
+			this.game.state.start('MainMenu');
+
+	}
+};
+
